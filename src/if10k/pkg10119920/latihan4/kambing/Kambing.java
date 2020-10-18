58cfc94a3c486113e10a244fa3ff14f67cf59d53
ursBot <umar.10119920@mahasiswa.ac.id>
2020-10-18 17:11:20 +0700

    upload pertama

---
+++ src/if10k/pkg10119920/latihan4/kambing/Kambing.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119920.latihan4.kambing;
+
+
+/**
+ *
+ * @author
+ * NAMA     : Umar Said Adi Pranoto
+ * KELAS    : IF10K
+ * NIM      : 10119920
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+
+public class Kambing {
+
+    public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
